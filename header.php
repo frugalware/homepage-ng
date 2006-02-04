8ec7a6012e2971dcba884ee529431a262fa8bafb
Krisztian VASAS <iron@frugalware.org>
2006-02-04 18:28:37 +0000

    title change - matrix-feeling started...

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="hu" lang="hu">
 <head>
-	<title>Frugalware Homepage - New Generation</title>
+	<title>Homepage - Reloaded</title>
 	<link rel="stylesheet" type="text/css" href="fwng.css" />
 	<meta http-equiv="Content-type" content="text/html; charset=iso-8859-2" />
 </head>
