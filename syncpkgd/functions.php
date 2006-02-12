ef2a94bdec5cae34ab9dc281a1784c7455a87df5
VMiklos <vmiklos@frugalware.org>
2006-02-12 02:50:27 +0000

    syncpkgd: created a template for the main page

--- syncpkgd/functions.php
+++ syncpkgd/functions.php
@@ -1,26 +1,4 @@
 <?
-function list_days()
-{
-	global $logdir;
-
-	display_header();
-	if ($dir = @opendir($logdir))
-	{
-		while ($file = readdir($dir))
-		{
-			if ($file != "." and $file != ".." and is_dir($logdir . "/" . $file))
-			{
-				print("<li><a href=\"$file\">$file</a></li>\n");
-			}
-		}
-	}
-	else
-	{
-		die('could not open $logdir, check your config.php!');
-	}
-	display_stats();
-}
-
 function display_day($day, $param=null)
 {
 	global $logdir;
@@ -68,34 +46,50 @@ function processtime($pid)
 	return($btime+$ptime[21]/100);
 }
 
-function display_stats()
+function list_days()
 {
-	print("</ul>\n");
-	print("General statistics:<br>\n");
+	global $logdir;
+
+	$host = trim(`hostname`) . "." . trim(`dnsdomainname`);
+	$arch = trim(`uname -m`);
+	$days = array();
+	if ($dir = @opendir($logdir))
+	{
+		while ($file = readdir($dir))
+		{
+			if ($file != "." and $file != ".." and is_dir($logdir . "/" . $file))
+			{
+				$days[] = "<li><a href=\"$file\">$file</a></li>\n";
+			}
+		}
+	}
+	else
+	{
+		die('could not open $logdir, check your config.php!');
+	}
 	$syncpkgdpid = trim(`ps -C syncpkgd -o pid=`);
 	if($syncpkgdpid!="")
-		printf("The package builder daemon is running, started on " .
-		date(DATE_RFC822, processtime($syncpkgdpid)) . ".<br>\n");
+	{
+		$syncd = true;
+		$syncddate = date(DATE_RFC822, processtime($syncpkgdpid));
+	}
 	else
-		printf("The package builder daemon is not running.<br>\n");
+		$syncd = false;
 	$syncpkgpid = trim(`ps -C syncpkg -o pid=`);
 	if($syncpkgpid!="")
-		printf("The package builder is running, started on " .
-		date(DATE_RFC822, processtime($syncpkgpid)) . ".<br>\n");
+		$sync = true;
+	else
+		$sync = false;
+	if($sync)
+		$syncdate = date(DATE_RFC822, processtime($syncpkgpid));
 	else
 	{
 		$syncpkgpid = trim(`ps -C "sleep 2h" -o pid=`);
-		printf("The package builder is sleeping, will be started again on " .
-		date(DATE_RFC822, processtime($syncpkgpid)+7200) . ".<br>\n");
+		$syncdate = date(DATE_RFC822, processtime($syncpkgpid)+7200);
 	}
 	$str = explode(" ", file_get_contents("/proc/loadavg"));
-	print("Current avarage for the past one minute: " . $str[0] . "<br>\n");
-}
+	$loadstat = $str[0];
 
-function display_header()
-{
-	printf("Welcome to " . trim(`hostname`) . "." . trim(`dnsdomainname`) . ", the " . trim(`uname -m`) .
-	" buildserver of <a href=\"http://frugalware.org\">Frugalware Linux</a>.<br>\n" .
-	"The following buildlogs are available:<br><ul>\n");
+	include("templates/daylist.php");
 }
 ?>
---
+++ syncpkgd/templates/daylist.php
@@ -0,0 +1,23 @@
+<?
+	printf("Welcome to $host, the $arch".
+	" buildserver of <a href=\"http://frugalware.org\">Frugalware Linux</a>.<br>\n" .
+	"The following buildlogs are available:<br><ul>\n");
+	foreach($days as $i)
+	{
+		print($i);
+	}
+	print("</ul>\n");
+	print("General statistics:<br>\n");
+	if($syncd)
+		print("The package builder daemon is running, started on " .
+		$syncddate . ".<br>\n");
+	else
+		print("The package builder daemon is not running.<br>\n");
+	if($sync)
+		print("The package builder is running, started on " .
+		$syncdate . ".<br>\n");
+	else
+		print("The package builder is sleeping, will be started again on " .
+		$syncdate . ".<br>\n");
+	print("Current avarage for the past one minute: " . $loadstat . "<br>\n");
+?>
