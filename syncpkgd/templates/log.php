5a3e99e90669ac525373ecbdee8b329240d29a6f
VMiklos <vmiklos@frugalware.org>
2006-02-16 00:09:46 +0000

    templates/log.php: html2ansi convert ansi escape sequences to html ones

--- syncpkgd/templates/log.php
+++ syncpkgd/templates/log.php
@@ -1,5 +1,40 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html>
+	<head>
+		<title>Buildlog</title>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+	</head>
+	<body style="background-color: black; color: white">
+		<pre>
 <?
-	// for now just dump the data as if it would be a static file
-	header("Content-type: text/plain");
-	include($log);
+$search = array(
+	'|\[1;1m(.*)\[1;0m|',
+	'|\[1;31m(.*)\[1;0m|',
+	'|\[1;32m(.*)\[1;0m|',
+	'|\[1;33m(.*)\[1;0m|',
+	'|\[1;34m(.*)\[1;0m|',
+	'|\[1;36m(.*)\[1;0m|',
+	'|&|');
+
+$replace = array(
+	'<b>\1</b>',
+	'<span style="color: red">\1</span>',
+	'<span style="color: green">\1</span>',
+	'<span style="color: yellow">\1</span>',
+	'<span style="color: blue">\1</span>',
+	'<span style="color: cyan">\1</span>',
+	'&amp;');
+
+	$fp = fopen($log, "r");
+	if($fp)
+	{
+		while (!feof($fp))
+		{
+			print(preg_replace($search, $replace, fgets($fp)));
+		}
+		fclose($fp);
+	}
 ?>
+		</pre>
+	</body>
+</html>
