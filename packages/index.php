fab90f2b55eff38fb9ee6fa8bbd93f5f308807ec
CSECSY Laszlo <boobaa@frugalware.org>
2006-03-19 13:36:19 +0000

    adding some more functionality to packages.fw.o   Usage is as follows (order matters):   Searching for a file:     if req begins with "file:" then that file is searched for, not the pkg     the beginning of the remainder is the filename to be searched for, ending before the first char which is not allowed in pkgname   Searching for a pkg:     if req begins with "sub:" then substring is checked, too (default: no)     if remainder begins with "desc:" then description is checked, too (default: no)     if remainder begins with "i686:" or "x86_64:" then that arch is checked (default: i686)     the beginning of the remainder is the string to be searched for, ending before the first char which is not allowed in pkgname

--- packages/index.php
+++ packages/index.php
@@ -1,5 +1,39 @@
-<html>
-<META HTTP-EQUIV="Refresh" CONTENT="1; URL=http://frugalware.org/packages.php?op=pkg&srch=<?php
-	print(preg_replace('|/([a-z0-9_+]+).*|','$1',$_SERVER['PATH_INFO']));
-?>&repo=all&arch=i686&fwver=current">
+<?php
+
+function bar($baz) {
+	return preg_replace('|/([a-z0-9_+-]+).*|','$1',$baz);
+}
+
+function foo() {
+	$url = 'http://frugalware.org/packages.php?op=';
+	$req = ltrim($_SERVER['PATH_INFO'],'/');
+	if(substr($req,0,5) == 'file:') {
+		return $url . 'file&amp;srch=' . bar(substr($req, 5)) . '&amp;repo=all';
+	}
+	$url .= 'pkg';
+	if(substr($req,0,4) == 'sub:') {
+		$url .= '&amp;sub=on';
+		$req = substr($req, 4);
+	}
+	if(substr($req,0,5) == 'desc:') {
+		$url .= '&amp;desc=on';
+		$req = substr($req, 5);
+	}
+	$url .= '&amp;arch=';
+	if(substr($req,0,7) == 'x86_64:') {
+		$url .= 'x86_64';
+		$req = substr($req, 7);
+	}
+	elseif(substr($req,0,5) == 'i686:') {
+		$url .= 'i686';
+		$req = substr($req, 5);
+	}
+	else {
+		$url .= 'i686';
+	}
+	$url .= '&amp;repo=all&amp;fwver=current&amp;srch=';
+	return $url . bar($req);
+}
+?><html>
+<META HTTP-EQUIV="Refresh" CONTENT="0; URL=<?php echo foo(); ?>">
 </html>
