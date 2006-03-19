c58914166be37443c3e87719a6c2da99e208517d
VMiklos <vmiklos@frugalware.org>
2006-03-19 13:28:55 +0000

    packages: initial import

---
+++ packages/index.php
@@ -0,0 +1,5 @@
+<html>
+<META HTTP-EQUIV="Refresh" CONTENT="1; URL=http://frugalware.org/packages.php?op=pkg&srch=<?php
+	print(preg_replace('|/([a-z0-9_+]+).*|','$1',$_SERVER['PATH_INFO']));
+?>&repo=all&arch=i686&fwver=current">
+</html>
