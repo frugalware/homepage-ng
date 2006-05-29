3c4bdee29e94279014e691e37b6c8ef020dcae0f
Krisztian VASAS <iron@frugalware.org>
2006-05-29 19:57:45 +0000

    fwng: new functions - copied needed xml files to xml/ dir - added authors.php - added rss.php - added screenshots.php   * added shots/ dir

---
+++ frugalware/rss.php
@@ -0,0 +1,138 @@
+<?php
+
+/**
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License v2 as published by
+ the Free Software Foundation
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ GNU General Public License for more details.
+ */
+
+/**
+ * Frugalware Linux Homepage - RSS feed
+ *
+ * @author Miklos Vajna <vmiklos@frugalware.org>
+ * @author Krisztian VASAS <iron@frugalware.org>
+ * @copyright Copyright (c) 2006. Krisztian VASAS
+ */
+
+include("config.inc.php");
+include("functions.inc.php");
+include("db.inc.php");
+
+switch($_GET['type'])
+{
+	case "stable":
+		$db = new FwDB();
+		$db->doConnect();
+
+		$handle['title']="Frugalware Linux";
+		$handle['desc']="Frugalware Linux is general purpose Linux distribution designed for intermediate users. Some of its elements were borrowed from Slackware Linux and Arch Linux.";
+		$handle['link']="http://frugalware.org/";
+		$query="select version, `desc` from releases where type='stable' order by date desc";
+		$result = $db->doQuery($query);
+		while ($i = $db->doFetchAssoc($result))
+		{
+			$handle['items'][] = array(
+				"title" => "frugalware-" . $i['version'],
+				"desc" => $i['desc'],
+				"link" => "http://frugalware.org/download.php?url=frugalware-" . $i['version'] . "-iso/frugalware-" . $i['version'] . "-dvd.iso"
+			);
+		}
+		$db->doClose();
+		break;
+
+	case "packages":
+		$db  = new FwDB();
+		$db->doConnect();
+
+		$handle['title']="Frugalware Linux Packages";
+		$handle['desc']="Latest updates to the Frugalware Linux package repositories.";
+		$handle['link']="http://frugalware.org/packages.php";
+		$query="select groups, pkgname, id, pkgver, pkgrel, arch, `desc`, unix_timestamp(updated), uploader from packages order by updated desc limit 10";
+		$result = $db->doQuery($query);
+		while ($i = $db->doFetchAssoc($result))
+		{
+			$handle['items'][] = array(
+				"title" => preg_replace("/^([^ ]*) .*/", "$1", $i['groups']) . "/${i['pkgname']}-${i['pkgver']}-${i['pkgrel']}-${i['arch']}",
+				"desc" => $i['desc'],
+				"author" => $i['uploader'],
+				"pubDate" => date(DATE_RFC2822, $i['unix_timestamp(updated)']),
+				"link" => "http://frugalware.org/packages.php?id=${i['id']}"
+			);
+		}
+		$db->doClose();
+		break;
+
+	case "darcs":
+		header('Content-Type: application/xml; charset=utf-8');
+		print(file_get_contents("http://darcs.frugalware.org/genesis.darcsweb/darcsweb.cgi?r=frugalware-current;a=rss"));
+		die();
+	case "bugs":
+		header('Content-Type: application/xml; charset=utf-8');
+		print(file_get_contents("http://bugs.frugalware.org/rss.php?type=new"));
+		die();
+	case "blogs":
+		header('Content-Type: application/xml; charset=utf-8');
+		print(file_get_contents("http://blogs.frugalware.org/xmlsrv/rss2.php?blog=1"));
+		die();
+	default:
+		include("header.php");
+?>
+<div id="columns">
+        <div id="leftcolumn">
+<?php
+	fwsidebox(gettext("Menu"), $menucontent);
+	fwsidebox(gettext("Languages"), $langcontent);
+?>
+        </div>
+        <div id="rightcolumn">
+<?php
+	fwsidebox(gettext("Information"), $validcontent);
+?>
+        </div>
+        <div id="centercolumn">
+<?php
+	fwmiddlebox("RSS",'<div align="left"><ul>
+			<li><a href="/rss.php?type=stable">Stable releases</a></li>
+			<li><a href="/rss.php?type=darcs">Darcs commits</a></li>
+			<li><a href="/rss.php?type=bugs">BTS entries</a></li>
+			<li><a href="/rss.php?type=packages">Package updates</a></li>
+			<li><a href="/rss.php?type=blogs">Blog posts</a></li>
+			</ul></div>'
+		);
+?>
+        </div>
+</div>
+<!-- main content end -->
+<?php
+		include("footer.php");
+		die();
+}
+
+header('Content-Type: application/xml; charset=utf-8');
+print("<?xml version=\"1.0\" encoding=\"utf-8\"?>
+<rss version=\"2.0\">
+<channel>
+	<title>".$handle['title']."</title>
+	<description>".$handle['desc']."</description>
+	<link>".$handle['link']."</link>\n"
+);
+foreach( $handle['items'] as $i )
+{
+	print("<item>\n<title>".$i['title']."</title>\n<description>".htmlspecialchars($i['desc'])."</description>\n<link>".$i['link']."</link>\n");
+	if(isset($i['author']))
+	{
+		print("<author>".$i['author']."</author>\n");
+	}
+	if(isset($i['pubDate']))
+	{
+		print("<pubDate>".$i['pubDate']."</pubDate>\n");
+	}
+	print("</item>\n");
+}
+print("</channel>\n</rss>");
+?>
---
+++ frugalware/screenshot.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License v2 as published by
+ the Free Software Foundation
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ GNU General Public License for more details.
+ */
+
+/**
+ * Frugalware Linux Homepage - New Generation
+ *
+ * @author Krisztian VASAS <iron@frugalware.org>
+ * @copyright Copyright (c) 2006. Krisztian VASAS
+ */
+
+// include some useful functions
+include("functions.inc.php");
+
+$lang = getlang();
+$llang = getllang($lang);
+
+// set the locale settings for gettext
+putenv("LANG=".$llang);
+setlocale(LC_ALL,$llang);
+$domain = 'messages';
+bindtextdomain($domain, "locale");
+textdomain($domain);
+
+// include the config and let's start page
+include("config.inc.php");
+include("shots/".$lang.".php");
+include("header.php");
+
+?>
+
+<!-- main content start -->
+<div id="columns">
+	<div id="leftcolumn">
+<?php
+fwsidebox(gettext("Menu"), $menucontent);
+fwsidebox(gettext("Languages"), $langcontent);
+?>
+	</div>
+
+	<div id="rightcolumn">
+<?php
+fwsidebox(gettext("Information"), $validcontent);
+?>
+	</div>
+
+	<div id="centercolumn">
+<?php
+
+$maincont = "<div class=\"screenshots\">\n";
+$maincont .= "<form>\n<fieldset class=\"fieldset\" id=\"installer\">\n<legend>Installer</legend>\n";
+for ($i=0; $i<count($shots[inst]); $i++)
+{
+	$maincont .= "<div><a href=\"http://www2.frugalware.org/images/screenshots/installer/".$shots[inst][$i][name]."\">".
+	"<img src=\"http://www2.frugalware.org/images/screenshots/installer/thumbnails/".$shots[inst][$i][name]."\"></a>".
+	"<br />".$shots[inst][$i][title]."</div>\n";
+}
+$maincont .= "</fieldset>\n<fieldset class=\"fieldset\" id=\"default\">\n";
+$maincont .= "<legend>Default Desktop</legend>\n";
+for ($i=0; $i<count($shots[defdesk]); $i++)
+{
+	$maincont .= "<div><a href=\"http://www2.frugalware.org/images/screenshots/default/".$shots[defdesk][$i][name]."\">".
+	"<img src=\"http://www2.frugalware.org/images/screenshots/default/thumbnails/".$shots[defdesk][$i][name]."\"></a>".
+	"<br />".$shots[defdesk][$i][title]."</div>\n";
+}
+$maincont .= "</form>\n</div>\n";
+
+#fwmiddlebox("", $maincont);
+print $maincont;
+
+?>
+	</div>
+
+</div>
+<!-- main content end -->
+
+<?php
+include("footer.php");
+?>
