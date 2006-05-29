2c27cde3162ab9b601559d38ea7aad7447ea18ba
Krisztian VASAS <iron@frugalware.org>
2006-05-29 20:04:33 +0000

    fwng: added missing authors.php

---
+++ frugalware/authors.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License v2 as published by
+ the Free Software Foundation
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ GNU General Public License for more details.
+ */
+
+/**
+ * Frugalware Linux Homepage - Authors' page
+ *
+ * @author Krisztian VASAS <iron@frugalware.org>
+ * @copyright Copyright (c) 2006. Krisztian VASAS
+ */
+
+// include some useful functions
+include("functions.inc.php");
+
+$lang = getlang();
+$llang = getllang($lang);
+$xmlfile = "xml/authors.xml";
+
+// set the locale settings for gettext
+putenv("LANG=".$llang);
+setlocale(LC_ALL,$llang);
+$domain = 'messages';
+bindtextdomain($domain, "locale");
+textdomain($domain);
+
+// include the config and let's start page
+include("config.inc.php");
+include("header.php");
+
+?>
+
+<!-- main content start -->
+<div id="columns">
+	<div id="leftcolumn">
+<?php
+fwsidebox(gettext("Menu"), $menucontent);
+fwsidebox(gettext("Languages"), $langcontent);
+?>
+	</div>
+
+	<div id="rightcolumn">
+<?php
+fwsidebox(gettext("Information"), $validcontent);
+?>
+	</div>
+
+	<div id="centercolumn">
+<?php
+
+include("xml.inc.php");
+$who = ($_GET['who'] != "") ? $_GET['who'] : "devel";
+
+switch($who)
+{
+	case "devel":
+		$xmlfile="xml/authors.xml";
+		$xml = file_get_contents($xmlfile);
+		$parser = new XMLParser($xml);
+		$parser->Parse();
+		$authors = "";
+		for ( $i=0; $i<count($parser->document->author); $i++)
+		{
+			$email = str_replace("@", " at ", $parser->document->author[$i]->email[0]->tagData);
+			$email = str_replace(".", " dot ", $email);
+			$authors .= "<p>\n".$parser->document->author[$i]->name[0]->tagData." (".$parser->document->author[$i]->nick[0]->tagData.") &lt;".$email."&gt;<br>\n<ul>\n";
+			for ( $j=0; $j<count($parser->document->author[$i]->role); $j++ )
+			{
+				$authors.= "<li>".$parser->document->author[$i]->role[$j]->tagData."</li>\n";
+			}
+			$authors .= "</ul>\n</p>\n";
+		}
+		$title = gettext("Developers");
+		break;
+
+	case "contrib";
+		$xmlfile="xml/contributors.xml";
+		$xml = file_get_contents($xmlfile);
+		$parser = new XMLParser($xml);
+		$parser->Parse();
+		$authors = "";
+		for ( $i=0; $i<count($parser->document->author); $i++)
+		{
+			$email = str_replace("@", " at ", $parser->document->author[$i]->email[0]->tagData);
+			$email = str_replace(".", " dot ", $email);
+			$authors .= "<p>\n".$parser->document->author[$i]->name[0]->tagData." (".$parser->document->author[$i]->nick[0]->tagData.") &lt;".$email."&gt;<br>\n<ul>\n";
+			for ( $j=0; $j<count($parser->document->author[$i]->role); $j++ )
+			{
+				$authors.= "<li>".$parser->document->author[$i]->role[$j]->tagData."</li>\n";
+			}
+			$authors .= "</ul>\n</p>\n";
+		}
+		$title = gettext("Contributors");
+		break;
+}
+fwmiddlebox($title, $authors);
+?>
+	</div>
+</div>
+<!-- main content end -->
+
+<?php
+include("footer.php");
+?>
