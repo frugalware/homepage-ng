30c65dd338c447b0022da2a69952a95cd7420174
alex_extreme <alex.extreme2@gmail.com>
2006-05-31 10:24:12 +0000

    fwng: rewritten roadmap.php to use roadmap.xml

--- frugalware/roadmap.php
+++ frugalware/roadmap.php
@@ -12,80 +12,94 @@
  */
 
 /**
- * Frugalware Linux Homepage - About (summary) page
+ * Frugalware Linux Homepage - Development roadmap page
  *
  * @author Krisztian VASAS <iron@frugalware.org>
  * @author Miklos Vajna <vmiklos@frugalware.org>
+ * @author Alex Smith <alex.extreme2@gmail.com>
  *
  * @copyright Copyright (c) 2006. Krisztian VASAS
  * @copyright Copyright (c) 2005-2006. Miklos Vajna
+ * @copyright Copyright (c) 2006. Alex Smith
  */
 
-// include some useful functions
-include("functions.inc.php");
+// Include some useful functions
+include('functions.inc.php');
 
+// Some definitions
 $lang = getlang();
 $llang = getllang($lang);
-$aboutfile = "includes/about.".$lang.".php";
+$xmlfile = 'xml/roadmap.xml';
 
-// set the locale settings for gettext
-putenv("LANG=".$llang);
-setlocale(LC_ALL,$llang);
+// Set the locale settings for gettext
+putenv("LANG=" . $llang);
+setlocale(LC_ALL, $llang);
 $domain = 'messages';
-bindtextdomain($domain, "locale");
+bindtextdomain($domain, 'locale');
 textdomain($domain);
 
-// include the config and let's start page
-include("config.inc.php");
-include("header.php");
+// Include the config and start the page
+include('config.inc.php');
+include('header.php');
 
-fwmiddlebox("0.5 (<acronym title=\"A planet prominent in Foundation and Foundation and Empire.\">Siwenna</acronym>)",
-"<table width=\"100%\">
-<tr><td>Sep 30, 2006<td>0.5<td><i>pending</i>
-<tr><td>Aug 31, 2006<td>0.5rc2<td><i>pending</i>
-<tr><td>Aug 17, 2006<td>0.5rc1<td><i>pending</i>
-<tr><td>Jul 20, 2006<td>0.5pre2<td><i>pending</i>
-<tr><td width=\"30%\">May 25, 2006<td width=\"30%\">0.5pre1<td width=\"30%\"><i>done</i>
-</table>
-");
+// This includes the XML parser
+include('xml.inc.php');
 
-fwmiddlebox("0.4 (<acronym title=\"The temporary headquarters for the navy of Bel Riose, during the war between the Empire and the Foundation.\">Wanda</acronym>)",
-"<table width=\"100%\">
-<tr><td>Mar 30, 2006<td>0.4<td><i>done</i>
-<tr><td>Mar 16, 2006<td>0.4rc2<td><i>done</i>
-<tr><td>Mar 2, 2006<td>0.4rc1<td><i>done</i>
-<tr><td>Feb 2, 2006<td>0.4pre2<td><i>done</i>
-<tr><td width=\"30%\">Dec 8, 2005<td width=\"30%\">0.4pre1<td width=\"30%\"><i>done</i>
-</table>
-");
+// Let's see whether the roadmap file exists or not
+if (!file_exists($xmlfile)) {
+	
+	echo(gettext('Sorry, a roadmap has not been written yet.'));
+	include('footer.php');
+	die();
+	
+}
 
-fwmiddlebox("0.3 (<acronym title=\"a fictional planet in Isaac Asimov's Foundation series and Empire series of science-fiction novels\">Trantor</acronym>)",
-"<table width=\"100%\">
-<tr><td>Oct 13, 2005<td>0.3<td><i>done</i>
-<tr><td>Oct 4, 2005<td>0.3rc2<td><i>done</i>
-<tr><td>Sep 16, 2005<td>0.3rc1<td><i>done</i>
-<tr><td>Aug 18, 2005<td>0.3pre2<td><i>done</i>
-<tr><td width=\"30%\">Jun 23, 2005<td width=\"30%\">0.3pre1<td width=\"30%\"><i>done</i>
-</table>
-");
+$xml = file_get_contents($xmlfile);
+$parser = new XMLParser($xml);
+$parser->Parse();
+$roadmap = $parser->document->release;
 
-fwmiddlebox("0.2 (<acronym title=\"a fictional planet in Isaac Asimov's Robot Series\">Aurora</acronym>)",
-"<table width=\"100%\">
-<tr><td>Apr 28, 2005<td>0.2<td><i>done</i>
-<tr><td>Apr 18, 2005<td>0.2rc2<td><i>done</i>
-<tr><td>Apr 7, 2005<td>0.2rc1<td><i>done</i>
-<tr><td>Feb 24, 2005<td>0.2pre2<td><i>done</i>
-<tr><td width=\"30%\">Jan 10, 2005<td width=\"30%\">0.2pre1<td width=\"30%\"><i>done</i>
-</table>
-");
+// The parser creates a too long and unuseful object hierarchy so create a better-readable one.
+for ( $i = 0; $i < count($roadmap); $i++) {
+	
+	$releases[$i][name] = $roadmap[$i]->name[0]->tagData;
+	$releases[$i][definition] = $roadmap[$i]->definition[0]->tagData;
+	$releases[$i][version] = $roadmap[$i]->version[0]->tagData;
+	$releases[$i][date] = date('M d, Y', strtotime($roadmap[$i]->date[0]->tagData));
+	if ($roadmap[$i]->status[0]->tagData == 1)
+		$releases[$i][status] = gettext('done');
+	else
+		$releases[$i][status] = gettext('pending');
+	
+	for ( $j=0; $j < count($roadmap[$i]->prerelease); $j++ ) {
+		
+		$releases[$i][prerelease][$j][preversion] = $roadmap[$i]->prerelease[$j]->preversion[0]->tagData;
+		$releases[$i][prerelease][$j][predate] = date('M d, Y', strtotime($roadmap[$i]->prerelease[$j]->predate[0]->tagData));
+		if ($roadmap[$i]->prerelease[$j]->prestatus[0]->tagData == 1)
+			$releases[$i][prerelease][$j][prestatus] = gettext('done');
+		else
+			$releases[$i][prerelease][$j][prestatus] = gettext('pending');
+		
+	}
+	
+}
 
-fwmiddlebox("0.1 (<acronym title=\"the first book of the Hebrew Bible\">Genesis</acronym>)",
-"<table width=\"100%\">
-<tr><td>Nov  2, 2004<td>0.1<td><i>done</i>
-<tr><td>Oct 11, 2004<td>0.1rc2<td><i>done</i>
-<tr><td width=\"30%\">Sep 29, 2004<td width=\"30%\">0.1rc1<td width=\"30%\"><i>done</i>
-</table>
-");
+for( $i=0; $i < count($releases); $i++ ) {
+	
+	$content = '<table width="100%">
+		<tr><td width="30%">' . $releases[$i][date] . '</td><td width="30%">' . $releases[$i][version] . '</td><td width="30%"><i>' . $releases[$i][status] . '</i></td></tr>';
+	
+	for ( $j=0; $j < count($releases[$i][prerelease]); $j++ ) {
+		
+		$content .= '<tr><td>' . $releases[$i][prerelease][$j][predate] . '</td><td>' . $releases[$i][version] . $releases[$i][prerelease][$j][preversion] . '</td><td>' . $releases[$i][prerelease][$j][prestatus] . '</td></tr>';
+		
+	}
+	
+	$content .= '</table>';
+	fwmiddlebox($releases[$i][version] . ' (<acronym title="' . $releases[$i][definition] . '">' . $releases[$i][name] . '</acronym>)',
+		$content);
+	
+}
 
 include("footer.php");
 ?>
