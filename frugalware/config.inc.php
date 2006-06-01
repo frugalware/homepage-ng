c0dadefe14626437d56277949cdd30104f5268ee
Krisztian VASAS <iron@frugalware.org>
2006-06-01 07:31:49 +0000

    fwng: another code cleanup and added irc.php - removed stupid "100%px" from fwng.css - cleaned $langcontent and infobox content - added irc.php

--- frugalware/config.inc.php
+++ frugalware/config.inc.php
@@ -22,9 +22,11 @@ $copydate = "2006";
 
 # box contents
 $validcontent = '<div align="center">
-	<a href="http://www2.frugalware.org/"><img src="' . $fwng_root . 'images/frugalware80x15.png" alt="Go Frugalware, Go" border="0" title="Go Frugalware, Go" /></a><br />
-	<a href="http://validator.w3.org/check/referer"><img src="' . $fwng_root . 'images/xhtml10.png" alt="Valid XHTML 1.0!" border="0" title="Valid XHTML 1.0!" /></a><br />
-	<a href="http://jigsaw.w3.org/css-validator/check/referer"><img src="' . $fwng_root . 'images/css.png" alt="Valid CSS!" title="Valid CSS!" border="0" /></a><!--br /><a href="http://feedvalidator.org/check.cgi?url=http://frugalware.org/rss2.php?lang=en"><img src="' . $fwng_root . 'images/valid-rss.png" border="0" alt="Valid RSS!" title="Valid RSS!" /></a--></div>' . "\n";
+	  <a href="http://www2.frugalware.org/"><img src="' . $fwng_root . 'images/frugalware80x15.png" alt="Go Frugalware, Go" border="0" title="Go Frugalware, Go" /></a><br />
+	  <a href="http://validator.w3.org/check/referer"><img src="' . $fwng_root . 'images/xhtml10.png" alt="Valid XHTML 1.0!" border="0" title="Valid XHTML 1.0!" /></a><br />
+	  <a href="http://jigsaw.w3.org/css-validator/check/referer"><img src="' . $fwng_root . 'images/css.png" alt="Valid CSS!" title="Valid CSS!" border="0" /></a><!--br />
+	  <a href="http://feedvalidator.org/check.cgi?url=http://frugalware.org/rss2.php?lang=en"><img src="' . $fwng_root . 'images/valid-rss.png" border="0" alt="Valid RSS!" title="Valid RSS!" /></a-->
+	</div>';
 
 $menucontent = '<a class="menu" href="index.php">' . gettext('News') . "</a> &middot; \n";
 $menucontent .= '<a class="menu" href="" onClick="return clickreturnvalue()" onMouseover="dropdownmenu(this, event, \'aboutmenu\')">' . gettext('About') . "</a> &middot; \n";
@@ -38,7 +40,7 @@ $menucontent .= '<div id="commmenu" class="menulinkcss" align="left">' . "\n";
 $menucontent .= '	<a href="http://frugalware.org/mailman/listinfo">' . gettext('Mailing Lists') . "</a>\n";
 $menucontent .= '	<a href="http://forums.frugalware.org/">' . gettext('Discussion Forums') . "</a>\n";
 $menucontent .= '	<a href="http://wiki.frugalware.org/">' . gettext('Wiki') . "</a>\n";
-$menucontent .= '	<a href="index.php">' . gettext('IRC') . "</a>\n";
+$menucontent .= '	<a href="irc.php">' . gettext('IRC') . "</a>\n";
 $menucontent .= '	<a href="screenshots.php">' . gettext('Screenshots') . "</a>\n";
 $menucontent .= '	<a href="http://www.frappr.com/frugalware">' . gettext('Map') . "</a>\n";
 $menucontent .= "</div>\n";
@@ -59,7 +61,11 @@ $menucontent .= '	<a href="authors.php?who=devel">' . gettext('Developers') . "<
 $menucontent .= '	<a href="authors.php?who=contrib">' . gettext('Contributors') . "</a>\n";
 $menucontent .= "</div>\n";
 
-$langcontent = '<div class="imgcontent"><a href="?lang=en"><img alt="' . gettext('Change language') . '" title="' . gettext('Change language') . '" src="' . $fwng_root . 'images/english.gif" border="0" /></a> | <a href="?lang=hu"><img alt="' . gettext('Change language') . '" title="' . gettext('Change language') . '" src="' . $fwng_root . 'images/hungarian.gif" border="0" /></a></div>';
+$langcontent = '<div class="imgcontent">
+	  <a href="?lang=en"><img alt="' . gettext('Change language') . '" title="' . gettext('Change language') . '" src="' . $fwng_root . 'images/english.gif" border="0" /></a>
+	  | 
+	  <a href="?lang=hu"><img alt="' . gettext('Change language') . '" title="' . gettext('Change language') . '" src="' . $fwng_root . 'images/hungarian.gif" border="0" /></a>
+	</div>';
 /*
 | <a href="?lang=es"><img alt="' . gettext('Change language') . '" title="' . gettext('Change language') . '" src="images/spanish.gif" border="0" /></a> | <a href="?lang=fr"><img alt="' . gettext('Change language') . '" title="' . gettext('Change language') . '" src="images/french.gif" border="0" /></a>';
 */
