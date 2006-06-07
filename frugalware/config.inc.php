277886375299a9b3e381ca68e0013a269a546969
VMiklos <vmiklos@frugalware.org>
2006-06-07 23:38:47 +0000

    fwng: use the new mod_rewrited urls in the language box & in the menu

--- frugalware/config.inc.php
+++ frugalware/config.inc.php
@@ -31,43 +31,47 @@ $validcontent = '<div align="center">
 	  <a href="http://feedvalidator.org/check.cgi?url=http://frugalware.org/rss2.php?lang=en"><img src="' . $fwng_root . 'images/valid-rss.png" border="0" alt="Valid RSS!" title="Valid RSS!" /></a-->
 	</div>';
 
-$menucontent = '<a class="menu" href="' . $fwng_root . 'index.php">' . gettext('News') . "</a> &middot; \n";
+$menucontent = '<a class="menu" href="' . $fwng_root . '">' . gettext('News') . "</a> &middot; \n";
 $menucontent .= '<a class="menu" href="" onClick="return clickreturnvalue()" onMouseover="dropdownmenu(this, event, \'aboutmenu\')">' . gettext('About') . "</a> &middot; \n";
 $menucontent .= '<div id="aboutmenu" class="menulinkcss" align="left">' . "\n";
-$menucontent .= '	<a href="' . $fwng_root . 'about.php">' . gettext('Summary') . "</a>\n";
-$menucontent .= '	<a href="' . $fwng_root . 'media.php">' . gettext('Frugalware in the Press') . "</a>\n";
-$menucontent .= '	<a href="' . $fwng_root . 'docs.php">' . gettext('Documentation') . "</a>\n";
+$menucontent .= '	<a href="' . $fwng_root . 'about">' . gettext('Summary') . "</a>\n";
+$menucontent .= '	<a href="' . $fwng_root . 'media">' . gettext('Frugalware in the Press') . "</a>\n";
+$menucontent .= '	<a href="' . $fwng_root . 'docs">' . gettext('Documentation') . "</a>\n";
 $menucontent .= "</div>\n";
 $menucontent .= '<a class="menu" href="" onClick="return clickreturnvalue()" onMouseover="dropdownmenu(this, event, \'commmenu\')">' . gettext('Community') . "</a> &middot; \n";
 $menucontent .= '<div id="commmenu" class="menulinkcss" align="left">' . "\n";
 $menucontent .= '	<a href="http://frugalware.org/mailman/listinfo">' . gettext('Mailing Lists') . "</a>\n";
 $menucontent .= '	<a href="http://forums.frugalware.org/">' . gettext('Discussion Forums') . "</a>\n";
 $menucontent .= '	<a href="http://wiki.frugalware.org/">' . gettext('Wiki') . "</a>\n";
-$menucontent .= '	<a href="' . $fwng_root . 'irc.php">' . gettext('IRC') . "</a>\n";
-$menucontent .= '	<a href="' . $fwng_root . 'screenshots.php">' . gettext('Screenshots') . "</a>\n";
+$menucontent .= '	<a href="' . $fwng_root . 'irc">' . gettext('IRC') . "</a>\n";
+$menucontent .= '	<a href="' . $fwng_root . 'screenshots">' . gettext('Screenshots') . "</a>\n";
 $menucontent .= '	<a href="http://www.frappr.com/frugalware">' . gettext('Map') . "</a>\n";
 $menucontent .= "</div>\n";
 $menucontent .= '<a class="menu" href="" onClick="return clickreturnvalue()" onMouseover="dropdownmenu(this, event, \'dlmenu\')">' . gettext('Download') . "</a> &middot; \n";
 $menucontent .= '<div id="dlmenu" class="menulinkcss" align="left">' . "\n";
-$menucontent .= '	<a href="' . $fwng_root . 'download.php">' . gettext('ISO images') . "</a>\n";
-$menucontent .= '	<a href="' . $fwng_root . 'packages.php">' . gettext('Packages') . "</a>\n";
+$menucontent .= '	<a href="' . $fwng_root . 'download">' . gettext('ISO images') . "</a>\n";
+$menucontent .= '	<a href="' . $fwng_root . 'packages">' . gettext('Packages') . "</a>\n";
 $menucontent .= "</div>\n";
 $menucontent .= '<a class="menu" href="" onClick="return clickreturnvalue()" onMouseover="dropdownmenu(this, event, \'develmenu\')">' . gettext('Development') . "</a>\n";
 $menucontent .= '<div id="develmenu" class="menulinkcss" align="left">' . "\n";
-$menucontent .= '	<a href="' . $fwng_root . 'roadmap.php">' . gettext('Roadmap') . "</a>\n";
+$menucontent .= '	<a href="' . $fwng_root . 'roadmap">' . gettext('Roadmap') . "</a>\n";
 $menucontent .= '	<a href="http://darcs.frugalware.org/">' . gettext('Darcs repository') . "</a>\n";
 $menucontent .= '	<a href="http://bugs.frugalware.org/">' . gettext('Bug Tracker') . "</a>\n";
-$menucontent .= '	<a href="' . $fwng_root . 'changelog.php">' . gettext('ChangeLog') . "</a>\n";
+$menucontent .= '	<a href="' . $fwng_root . 'changelog">' . gettext('ChangeLog') . "</a>\n";
 $menucontent .= '	<a href="http://blogs.frugalware.org/">' . gettext('Blogs') . "</a>\n";
-$menucontent .= '	<a href="' . $fwng_root . 'donations.php">' . gettext('Donations') . "</a>\n";
-$menucontent .= '	<a href="' . $fwng_root . 'authors.php?who=devel">' . gettext('Developers') . "</a>\n";
-$menucontent .= '	<a href="' . $fwng_root . 'authors.php?who=contrib">' . gettext('Contributors') . "</a>\n";
+$menucontent .= '	<a href="' . $fwng_root . 'donations">' . gettext('Donations') . "</a>\n";
+$menucontent .= '	<a href="' . $fwng_root . 'authors/devel">' . gettext('Developers') . "</a>\n";
+$menucontent .= '	<a href="' . $fwng_root . 'authors/contrib">' . gettext('Contributors') . "</a>\n";
 $menucontent .= "</div>\n";
 
+if($_SERVER["PHP_SELF"]=="/index.php")
+	$langpage=$fwng_root;
+else
+	$langpage=$fwng_root . substr($_SERVER["PHP_SELF"], 1, strlen($_SERVER["PHP_SELF"])-5) . "/";
 $langcontent = '<div class="imgcontent">
-	  <a href="?lang=en"><img alt="' . gettext('Change language') . '" title="' . gettext('Change language') . '" src="' . $fwng_root . 'images/english.gif" border="0" /></a>
+	  <a href="' . $langpage . 'en"><img alt="' . gettext('Change language') . '" title="' . gettext('Change language') . '" src="' . $fwng_root . 'images/english.gif" border="0" /></a>
 	  | 
-	  <a href="?lang=hu"><img alt="' . gettext('Change language') . '" title="' . gettext('Change language') . '" src="' . $fwng_root . 'images/hungarian.gif" border="0" /></a>
+	  <a href="' . $langpage . 'hu"><img alt="' . gettext('Change language') . '" title="' . gettext('Change language') . '" src="' . $fwng_root . 'images/hungarian.gif" border="0" /></a>
 	</div>';
 /*
 | <a href="?lang=es"><img alt="' . gettext('Change language') . '" title="' . gettext('Change language') . '" src="images/spanish.gif" border="0" /></a> | <a href="?lang=fr"><img alt="' . gettext('Change language') . '" title="' . gettext('Change language') . '" src="images/french.gif" border="0" /></a>';
