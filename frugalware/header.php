6ed4cd92d66957e0296c90fcdfeb9e5bdff6aa2f
alex_extreme <alex.extreme2@gmail.com>
2006-06-16 19:31:27 +0000

    fwng: hopefully fix that validation error

--- frugalware/header.php
+++ frugalware/header.php
@@ -27,7 +27,7 @@ for ( $i=0; $i<count($stable); $i++ )
 {
 	$rels .= "<p><a href=\"".$fwng_root."news/".$stable[$i][newsid]."\">frugalware-".$stable[$i][version]." (".$stable[$i][name].")</a><br />".$stable[$i][date]."</p>\n";
 }
-$rels .= "<p>\n<div align=\"center\"><a href=\"${fwng_root}rss/releases\">RSS</a></div>\n</p>\n";
+$rels .= "<p>\n<span style=\"text-align: center\"><a href=\"${fwng_root}rss/releases\">RSS</a></span>\n</p>\n";
 
 include("db.inc.php");
 if(file_exists($pkgcache))
