db7302fcf7bde3108930dc4166165dafa3f42f28
VMiklos <vmiklos@frugalware.org>
2006-06-26 23:18:40 +0000

    darcs/patches: initial import

---
+++ darcs/patches/index.php
@@ -0,0 +1,16 @@
+<?
+$repo = substr($_SERVER["PATH_INFO"], 1);
+if (strpos($_SERVER["PATH_INFO"], "/") !== false)
+	$repo = preg_replace('|^([^/]*)/.*|', '$1', $repo);
+
+$patch = preg_replace('|^[^/]*/([^/]*)|', '$1',
+	substr($_SERVER["PATH_INFO"], 1));
+if($patch==$repo)
+	$patch=null;
+
+if ($repo == "")
+	die();
+$patch = substr($patch, 0, strlen($patch)-6);
+header('Content-Type: text/plain; charset=utf8');
+include("http://darcs.frugalware.org/darcsweb/darcsweb.cgi?r=$repo;a=plain_commitdiff;h=$patch.gz;");
+?>
