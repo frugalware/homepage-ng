2a99e29278bc30b9ff982e9b8832a445c51c3d8e
AlexExtreme <alex@alex-smith.me.uk>
2006-10-21 10:53:39 +0000

    homepage: added security page (part 1)

---
+++ frugalware/security.php
@@ -0,0 +1,92 @@
+<?php
+
+/**
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License v2 as published by
+ the Free Software Foundation
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ GNU General Public License for more details.
+ */
+
+/**
+ * Frugalware Linux Homepage - New Generation
+ *
+ * @author Alex Smith <alex@alex-smith.me.uk>
+ * @copyright Copyright (C) 2006 Alex Smith
+ */
+
+// Include some useful functions and the config
+include('functions.inc.php');
+
+$lang = getlang();
+$llang = getllang($lang);
+
+// Set the locale settings for gettext
+$domain = "homepage";
+set_locale($llang, $domain);
+
+// Let's start the page
+include('config.inc.php');
+include('header.php');
+
+// This includes the XML parser
+include('xml.inc.php');
+
+// Let's see whether the security file exist or not
+$flang = ( $lang == 'en' ) ? "" : "_$lang";
+if (file_exists('xml/security.xml'))
+	$xmlfile = 'xml/security.xml';
+else
+	$xmlfile = $docs_path.'xml/media.xml';
+if (!file_exists($xmlfile)) {
+	
+	echo(gettext('Sorry, a security file has not been written yet.'));
+	include('footer.php');
+	die();
+	
+}
+
+fwmiddlebox(gettext('Frugalware Security Announcements (FSAs)'),
+	gettext('This is a list of security announcments that have been released for the current stable version of Frugalware'));
+
+$xml = file_get_contents($xmlfile);
+$parser = new XMLParser($xml);
+$parser->Parse();
+$security = $parser->document->fsa;
+
+// The parser creates too long and unuseful object hierarchy, so create a better-readable one.
+for ( $i = 0; $i < count($security); $i++) {
+	
+	$fsas[$i][id] = $security[$i]->id[0]->tagData;
+	$fsas[$i][date] = $security[$i]->date[0]->tagData;
+	$fsas[$i][author] = $security[$i]->author[0]->tagData;
+	$fsas[$i][package] = $security[$i]->package[0]->tagData;
+	$fsas[$i][vulnerable] = $security[$i]->vulnerable[0]->tagData;
+	$fsas[$i][unaffected] = $security[$i]->unaffected[0]->tagData;
+	$fsas[$i][bts] = $security[$i]->bts[0]->tagData;
+	$fsas[$i][cve] = $security[$i]->cve[0]->tagData;
+	$fsas[$i][desc] = $security[$i]->desc[0]->tagData;
+	
+}
+
+// Let's write out details of each FSA in a separate box
+for( $i = 0; $i < count($fsas); $i++ ) {
+	
+	fwmiddlebox('FSA' . $fsas[$i][id] . ' - ' . $fsas[$i][package],
+		'<table width="100%">
+		<tr><td width="30%">' . gettext('Package') . ':</td><td width="70%">' . $fsas[$i][package] . '</td></tr>
+		<tr><td>' . gettext('Date') . ':</td><td>' . $fsas[$i][date] . '</td></tr>
+		<tr><td>' . gettext('Posted by') . ':</td><td>' . $fsas[$i][author] . '</td></tr>
+		<tr><td>' . gettext('Vulnerable version') . ':</td><td>' . $fsas[$i][vulnerable] . '</td></tr>
+		<tr><td>' . gettext('Unaffected version') . ':</td><td>' . $fsas[$i][unaffected] . '</td></tr>
+		<tr><td>' . gettext('Bug tracker entry') . ':</td><td><a href="' . $fsas[$i][bts] . '">' . $fsas[$i][bts] . '</a></td></tr>
+		<tr><td>' . gettext('CVEs') . ':</td><td>' . $fsas[$i][cve] . '</td></tr>
+		<tr><td>' . gettext('Description') . ':</td><td>' . $fsas[$i][desc] . '</td></tr></table>');
+	
+}
+
+include("footer.php");
+?>
