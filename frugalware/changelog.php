e9cb0276f2f212792a640ce551df333d8247e8ce
AlexExtreme <alex@alex-smith.me.uk>
2006-11-25 09:51:41 +0000

    homepage: fix changelog.php

--- frugalware/changelog.php
+++ frugalware/changelog.php
@@ -1,7 +1,37 @@
 <?php
+
+/**
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License v2 as published by
+ the Free Software Foundation
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ GNU General Public License for more details.
+ */
+
+/**
+ * Frugalware Linux Homepage - Package and file search page
+ *
+ * @author Krisztian VASAS <iron@frugalware.org>
+ * @author Miklos Vajna <vmiklos@frugalware.org>
+ *
+ * @copyright Copyright (c) 2006. Krisztian VASAS
+ * @copyright Copyright (c) 2005-2006. Miklos Vajna
+ */
+
+// include some useful functions
 include("functions.inc.php");
+
+$lang = getlang();
+$llang = getllang($lang);
+
+// set the locale settings for gettext
 $domain = "homepage";
-set_locale(getllang(getlang()), $domain);
+set_locale($llang, $domain);
+
+// include the config and let's start page
 include("config.inc.php");
 include("header.php");
 
