3392cd788a30a7f7d0e2a4504c35f296c8dfa8ff
VMiklos <vmiklos@frugalware.org>
2006-12-30 16:47:02 +0000

    frugalware: reworked the download page to use mirrors2.xml

--- frugalware/download.php
+++ frugalware/download.php
@@ -35,69 +35,46 @@ include("config.inc.php");
 include("header.php");
 include("xml.inc.php");
 
-if (file_exists("xml/mirrors.xml"))
-	$mirrorsfile = "xml/mirrors.xml";
+if (file_exists("xml/mirrors2.xml"))
+	$mirrorsfile = "xml/mirrors2.xml";
 else
-	$mirrorsfile = $docs_path."/xml/mirrors.xml";
-$mirr = file_get_contents($mirrorsfile);
-$parser = new XMLParser($mirr);
+	$mirrorsfile = $docs_path."/xml/mirrors2.xml";
+$xml = file_get_contents($mirrorsfile);
+$parser = new XMLParser($xml);
 $parser->Parse();
+$mirrors = array();
 for ( $i=0; $i<count($parser->document->mirror); $i++)
 {
-	$mirrors[$i][name] = $parser->document->mirror[$i]->name[0]->tagData;
-	$mirrors[$i][type] = $parser->document->mirror[$i]->type[0]->tagData;
-	$mirrors[$i][path] = $parser->document->mirror[$i]->path[0]->tagData;
-	$mirrors[$i][desc] = $parser->document->mirror[$i]->description[0]->tagData;
+	$mirror = array();
+	$mirror['id'] = $parser->document->mirror[$i]->id[0]->tagData;
+	for($j=0; $j<count($parser->document->mirror[$i]->type);$j++)
+		$mirror['types'][] = $parser->document->mirror[$i]->type[$j]->tagData;
+	$mirror['path'] = $parser->document->mirror[$i]->path[0]->tagData;
+	$mirror['country'] = $parser->document->mirror[$i]->country[0]->tagData;
+	$mirror['supplier'] = $parser->document->mirror[$i]->supplier[0]->tagData;
+	$mirror['bandwidth'] = $parser->document->mirror[$i]->bandwidth[0]->tagData;
+	$mirrors[$mirror['country']][] = $mirror;
 }
+ksort($mirrors);
 
-$ftpcont = $httpcont = $rsynccont = "<table width=\"100%\" border=\"0\">\n";
-$buycdcont = gettext("You can buy Frugalware CDs at the following sites:")."<ul>\n";
 $url = addslashes(stripslashes($_GET[url]));
-#$url = $_GET[url];
 
-for ( $i=0; $i<count($mirrors); $i++ )
+foreach($mirrors as $k => $v)
 {
-	if ( $mirrors[$i][type] == "ftp" )
+	$str = null;
+	if($k == "")
+		continue;
+	foreach($v as $i)
 	{
-		$ftpcont .= "<tr><td width=\"50%\"><a href=\"ftp://".$mirrors[$i][name]."/".$mirrors[$i][path]."/$url\">".$mirrors[$i][name]."</a></td><td>".$mirrors[$i][desc]."</td></tr>\n";
+		foreach($i['types'] as $j)
+		{
+			$j=="http" ? $domain = "www" : $domain = $j;
+			$str .= "<a href=\"".$j."://".$domain.$i['id'].".frugalware.org/".$i['path']."/$url\">".
+				$i['supplier']." (".$j."/".$i['bandwidth'].")</a><br />\n";
+		}
 	}
-	if ( $mirrors[$i][type] == "http" )
-	{
-		$httpcont .= "<tr><td width=\"50%\"><a href=\"http://".$mirrors[$i][name]."/".$mirrors[$i][path]."/$url\">".$mirrors[$i][name]."</a></td><td>".$mirrors[$i][desc]."</td></tr>\n";
-	}
-	if ( $mirrors[$i][type] == "rsync" )
-	{
-		$rsynccont .= "<tr><td width=\"50%\"><a href=\"rsync://".$mirrors[$i][name]."/".$mirrors[$i][path]."/$url\">".$mirrors[$i][name]."</a></td><td>".$mirrors[$i][desc]."</td></tr>\n";
-	}
-	if ( $mirrors[$i][type] == "buycd" )
-	{
-		$buycdcont .= "<li><a href=\"" . htmlentities($mirrors[$i][path]). "\">".$mirrors[$i][name]."</a></li>\n";
-	}
-}
-$ftpcont .= "</table>\n";
-$httpcont .= "</table>\n";
-$rsynccont .= "</table>\n";
-$buycdcont .= "</ul>\n";
+	fwmiddlebox($k, $str);
 
-$howto = "<div align=\"left\"><ul>
-	<li>".gettext("Full mirroring (~27GB)").":<br />
-	<br /><tt>rsync -avP --delete-after <i>server</i> /download/directory</tt><br />&nbsp;</li>
-	<li>".gettext("Only a specified tree (~5GB, for example -current)").":<br />
-	<br /><tt>rsync -avP --delete-after <i>server</i>/frugalware-current/ /download/directory/location </tt><br />&nbsp;</li>
-</ul></div>";
-if ( $url != "" )
-{
-	fwmiddlebox(gettext("Download file (via ftp)"), $ftpcont);
-	fwmiddlebox(gettext("Download file (via http)"), $httpcont);
-	fwmiddlebox(gettext("Download file (via rsync)"), $rsynccont);
-}
-else
-{
-	fwmiddlebox(gettext("Via FTP"), $ftpcont);
-	fwmiddlebox(gettext("Via HTTP"), $httpcont);
-	fwmiddlebox(gettext("Via Rsync"), $rsynccont);
-	fwmiddlebox(gettext("Rsync mini-howto"), $howto);
-	fwmiddlebox(gettext("Get a CD"), $buycdcont);
 }
 
 include("footer.php");
