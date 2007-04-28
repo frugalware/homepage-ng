889ab5bb59ca9ac3bbeaf7fd05e453c2feb7304f
AlexExtreme <alex@alex-smith.me.uk>
2007-04-28 19:54:25 +0000

    pastebin: fix syntax errror...

--- pastelib/pastebin/pastebin.class.php
+++ pastelib/pastebin/pastebin.class.php
@@ -163,7 +163,7 @@ class Pastebin
 			if (strlen($poster)==0)
 				$poster='Anonymous';
 			
-			if (preg_match($this->config['spam_regex'], $poster)
+			if (preg_match($this->config['spam_regex'], $poster))
 				die('Spam detected. Nice try.');
 			
 			$format=$post['format'];
@@ -172,7 +172,7 @@ class Pastebin
 			
 			$code=$post["code2"];
 			
-			if (preg_match($this->config['spam_regex'], $code)
+			if (preg_match($this->config['spam_regex'], $code))
 				die('Spam detected. Nice try.');
 			
 			//now insert..
