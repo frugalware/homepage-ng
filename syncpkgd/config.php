ccaddacf4a3d4261adbdfc982f50ab78fe4df9ef
VMiklos <vmiklos@frugalware.org>
2007-05-25 22:20:55 +0000

    syncpkg/config: syntax fix

--- syncpkgd/config.php
+++ syncpkgd/config.php
@@ -1,6 +1,6 @@
 <?
 $logdir="/var/log/syncpkg";
 $providers = array("factory" => "University of Debrecen",
-	"helicon" => "TeleDataCast Kft.");
+	"helicon" => "TeleDataCast Kft.",
 	"yugo" => "The Computer and Automation Research Institute, Hungarian Academy of Sciences.");
 ?>
