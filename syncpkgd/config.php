395eed77fbb29991936bc7136421b71fc94dbf01
VMiklos <vmiklos@frugalware.org>
2007-05-25 22:18:06 +0000

    syncpkgd/config: add yugo

--- syncpkgd/config.php
+++ syncpkgd/config.php
@@ -2,4 +2,5 @@
 $logdir="/var/log/syncpkg";
 $providers = array("factory" => "University of Debrecen",
 	"helicon" => "TeleDataCast Kft.");
+	"yugo" => "The Computer and Automation Research Institute, Hungarian Academy of Sciences.");
 ?>
