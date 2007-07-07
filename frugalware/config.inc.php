ec3cf88a888488aa2760c1a809f58cc1b4877695
AlexExtreme <alex@alex-smith.me.uk>
2007-07-07 07:04:06 +0100

    homepage: added git.frugalware.org to navigation menu

--- frugalware/config.inc.php
+++ frugalware/config.inc.php
@@ -71,6 +71,7 @@ $menucontent .= '<a class="menu" href="" onclick="return clickreturnvalue()" onm
 $menucontent .= '<div id="develmenu" class="menulinkcss" align="left">' . "\n";
 $menucontent .= '	<a href="' . $fwng_root . 'roadmap">' . gettext('Roadmap') . "</a>\n";
 $menucontent .= '	<a href="http://darcs.frugalware.org/">' . gettext('Darcs repository') . "</a>\n";
+$menucontent .= '	<a href="http://git.frugalware.org/">' . gettext('Git repository') . "</a>\n";
 $menucontent .= '	<a href="http://bugs.frugalware.org/">' . gettext('Bug Tracker') . "</a>\n";
 $menucontent .= '	<a href="http://pootle.frugalware.org/">' . gettext('Translation portal') . "</a>\n";
 $menucontent .= '	<a href="' . $fwng_root . 'changelog">' . gettext('ChangeLog') . "</a>\n";
