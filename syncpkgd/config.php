9f47c4d50f94e6ac99b59dbb962fb0d2585e55c5
VMiklos <vmiklos@frugalware.org>
2007-08-08 11:19:46 +0200

    syncpkgd: redirect to the buildlogs page also remove the old code

--- syncpkgd/config.php
+++
@@ -1,6 +0,0 @@
-<?
-$logdir="/var/log/syncpkg";
-$providers = array("factory" => "University of Debrecen",
-	"helicon" => "TeleDataCast Kft.",
-	"yugo" => "The Computer and Automation Research Institute, Hungarian Academy of Sciences.");
-?>
--- syncpkgd/index.php
+++ syncpkgd/index.php
@@ -1,20 +1,4 @@
 <?
-include("config.php");
-include("functions.php");
-
-$day = substr($_SERVER["PATH_INFO"], 1);
-if (strpos($_SERVER["PATH_INFO"], "/") !== false)
-	$day = preg_replace('|^([^/]*)/.*|', '$1', $day);
-
-$param = preg_replace('|^[^/]*/([^/]*)|', '$1',
-	substr($_SERVER["PATH_INFO"], 1));
-if($param==$day)
-	$param=null;
-
-if ($day == "")
-	list_days();
-else if($day == "hardware")
-	query_hardware();
-else
-	display_day($day, $param);
+$hostname = explode(".", $_SERVER['SERVER_NAME']);
+header("Location: http://frugalware.org/buildlogs/" . $hostname[0]);
 ?>
