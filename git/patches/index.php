d17ecdf661c6200b79a04c832bc4dff9cf138154
VMiklos <vmiklos@frugalware.org>
2007-10-05 01:59:23 +0200

    git/patches: fix for newer gitweb

--- git/patches/index.php
+++ git/patches/index.php
@@ -12,5 +12,5 @@ if ($repo == "")
 	die("usage: http://git.frugalware.org/patches/project/hash.patch");
 $patch = substr($patch, 0, strlen($patch)-6);
 header('Content-Type: text/plain; charset=utf8');
-include("http://git.frugalware.org/gitweb/gitweb.cgi?p=$repo/.git;a=commitdiff_plain;h=$patch");
+include("http://git.frugalware.org/gitweb/gitweb.cgi?p=$repo.git;a=commitdiff_plain;h=$patch");
 ?>
