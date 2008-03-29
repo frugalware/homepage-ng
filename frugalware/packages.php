e82e479032b9e9db42407203fadc039e1ae850ba
Miklos Vajna <vmiklos@frugalware.org>
2008-03-29 01:30:51 +0100

    packages: mark 3 new strings as translatable

--- frugalware/packages.php
+++ frugalware/packages.php
@@ -90,10 +90,10 @@ function main()
 		$content = "<form name=\"pkgsrch\" action=\"/packages/\" method=\"get\">
 		<fieldset class=\"pkg\"><legend>".gettext("Package search")."</legend>
 		".gettext("Search for:")."<br />
-		<input type=\"radio\" name=\"op\" value=\"pkg\" checked=\"checked\"/>packages<br />
+		<input type=\"radio\" name=\"op\" value=\"pkg\" checked=\"checked\"/>".gettext("packages")."<br />
 		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input class=\"required\" type=\"checkbox\" name=\"desc\" id=\"descr\" /><label for=\"descr\" class=\"pkg-phrasing\">".gettext("Search in description")."</label><br />
-		<input type=\"radio\" name=\"op\" value=\"file\" />files<br />
-		<input type=\"radio\" name=\"op\" value=\"groups\" />groups<br />
+		<input type=\"radio\" name=\"op\" value=\"file\" />".gettext("files")."<br />
+		<input type=\"radio\" name=\"op\" value=\"groups\" />".gettext("groups").<br />
 		<input class=\"required\" type=\"text\" id=\"pkgsrc\" name=\"srch\" size=\"40\" title=\"".gettext("Regular expression")."\"/> " . gettext( '(regular expression)' ) . "
 		<br />
 		<br />
