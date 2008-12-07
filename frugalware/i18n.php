787da8806d92194dfdb058a3feb238937dbd8b00
Devil505 <devil505linux@gmail.com>
2008-12-07 12:59:07 +0100

    * removed spanish website (dead) * changed frugalware-fr link (now wwww.frugalware-fr.org)

--- frugalware/i18n.php
+++ frugalware/i18n.php
@@ -32,8 +32,7 @@ fwmiddlebox("Internationalized Frugalware sites",'<ul>
 	<li><a href="http://frugalware-br.org//">' . gettext('Brazilian') . '</a></li>
 	<li><a href="http://www.frugalware.hostend.eu/">' . gettext('Czech') . '</a></li>
 	<li><a href="http://frugalware.dk/">' . gettext('Danish') . '</a></li>
-	<li><a href="http://frugalware-fr.tuxfamily.org/">' . gettext('French') . '</a></li>
-	<li><a href="http://frugalware-es.tuxfamily.org/">' . gettext('Spanish') . '</a></li>
+	<li><a href="http://www.frugalware-fr.org/">' . gettext('French') . '</a></li>
 	</ul>'
 );
 include("footer.php");
