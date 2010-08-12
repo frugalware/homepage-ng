ef388c3507f47eb4116ac18d3d48af72c7d87afa
Devil505 <devil505linux@gmail.com>
2010-08-12 08:43:43 +0200

    * just using html

--- frugalware/packages.php
+++ frugalware/packages.php
@@ -139,7 +139,7 @@ function addEngine()
 }
 </script>
 
-<div align=\"center\">" . gettext("Click <a href=\"http://frugalware.org/~vmiklos/stats/chkworld.html">here</a> to see the chkworld page.") . "</div>&nbsp;\n";
+<div align="center"> Click <a href=\"http://frugalware.org/~vmiklos/stats/chkworld.html">here</a> to see the chkworld page. </div>&nbsp;
 
 <div align=\"center\">" . gettext("Click <a href=\"javascript:addEngine()\">here</a> to install the Firefox search plugin.") . "</div>&nbsp;\n";
 		print $content;
