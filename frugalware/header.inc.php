93e839628e0312deb8a057742d8750087c83ecf5
Krisztian VASAS <iron@frugalware.org>
2010-08-31 15:42:41 +0200

    fwng: removed trailing whitespaces in header.inc.php

--- frugalware/header.inc.php
+++ frugalware/header.inc.php
@@ -22,7 +22,7 @@ include("config.inc.php");
 include("db.inc.php");
 
 function genHeader($usegettext = true) {
-	
+
 	global $fwng_root;
 	global $sqltype;
 	global $sqlhost;
@@ -33,103 +33,103 @@ function genHeader($usegettext = true) {
 	global $pkgcachetimeout;
 	global $upfile;
 	global $docs_path;
-	
+
 	// Find where the XML roadmap file is
 	if (file_exists('xml/roadmap.xml'))
 		$xmlfl = 'xml/roadmap.xml';
 	else
 		$xmlfl = $docs_path.'xml/roadmap.xml';
-	
+
 	// And read it in
 	$xml = file_get_contents($xmlfl);
 	$parser = new XMLParser($xml);
 	$parser->Parse();
 	$roadmap = $parser->document->release;
-	
+
 	// Produce a better readable structure
 	$j = 0;
 	for ( $i = 0; $i < count($roadmap); $i++) {
-		
+
 		if ($roadmap[$i]->status[0]->tagData == 1) {
-			
+
 			$stable[$j][name] = $roadmap[$i]->name[0]->tagData;
 			$stable[$j][version] = $roadmap[$i]->version[0]->tagData;
 			$stable[$j][date] = $roadmap[$i]->date[0]->tagData;
 			$stable[$j][newsid] = $roadmap[$i]->newsid[0]->tagData;
 			$j++;
-			
+
 		}
-		
+
 	}
-	
+
 	// Create the info
 	$rels = "";
 	for ( $i=0; $i<count($stable); $i++ ) {
-		
+
 		$rels .= "<p><a href=\"".$fwng_root."news/".$stable[$i][newsid]."\">frugalware-".$stable[$i][version]." (".$stable[$i][name].")</a><br />".$stable[$i][date]."</p>\n";
-		
+
 	}
-	
+
 	$rels .= "<p align=\"center\">\n<a href=\"${fwng_root}rss/releases\">RSS</a>\n</p>\n";
-	
+
 	if(file_exists($pkgcache))
 		$info = stat($pkgcache);
-	
+
 	if(!(isset($info) && ((time() - $info["mtime"])<$pkgcachetimeout))) {
-		
+
 		$db = new FwDB();
 		$db->doConnect($sqlhost, $sqluser, $sqlpass, $sqldb);
-		
-		$query = "select packages.pkgname, groups.name, packages.id, 
-			packages.pkgver, packages.arch, packages.`desc`, 
-			unix_timestamp(packages.builddate) from packages, groups, 
-			ct_groups where packages.id = ct_groups.pkg_id and 
-			ct_groups.group_id = groups.id group by 
+
+		$query = "select packages.pkgname, groups.name, packages.id,
+			packages.pkgver, packages.arch, packages.`desc`,
+			unix_timestamp(packages.builddate) from packages, groups,
+			ct_groups where packages.id = ct_groups.pkg_id and
+			ct_groups.group_id = groups.id group by
 			concat(packages.pkgname, packages.arch, fwver) order by
 			packages.builddate desc limit 10";
 		$result = $db->doQuery($query);
-		
+
 		while($i = $db->doFetchRow($result))
 			$pkgs[] = $i;
-		
+
 		$db->doClose();
 		$fp = fopen($pkgcache, "w");
 		fwrite($fp, "<div align=\"left\">\n");
-		
+
 		foreach($pkgs as $i) {
-			
+
 			$writeout = $i['name'] . "/${i['pkgname']}";
 			if (strlen($writeout) > 20)
 				$writeout = $i['name'] . "/<br />&nbsp;${i['pkgname']}";
 			fwrite($fp, $writeout."<br />\n" .
 				"<a href=\"${fwng_root}packages/${i['id']}\">${i['pkgver']}-${i['arch']}</a><br />\n");
-			
+
 		}
-		
+
 		fwrite($fp, "</div>");
 		fwrite($fp, "<br />\n<div align=\"center\"><a href=\"${fwng_root}rss/packages\">RSS</a></div>");
 		fclose($fp);
-		
+
 	}
-	
+
 	$recupd = file_get_contents($pkgcache);
-	
+
 	$statf = file($upfile);
 	list($uptime, $junk) = explode(" ", $statf[0]);
 	$secuptime=floor($uptime);
-	
+
 	// Seconds
 	$minuptime=60*floor($uptime/60);
 	$sec= $secuptime - $minuptime;
-	
+
 	// Minutes
 	$houruptime=3600*floor($minuptime/3600);
 	$min= $minuptime - $houruptime;
-	
+
 	// Hours
 	$dayuptime=86400*floor($houruptime/86400);
 	$hour= $houruptime - $dayuptime;
-	
+
 	// Final uptime
 	if ($usegettext)
 		$uptime = sprintf(gettext("Uptime:<br /> %d day(s) %d h %d m %d s"), $dayuptime/86400, $hour/3600, $min/60, $sec);
