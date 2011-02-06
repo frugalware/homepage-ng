67294766731a4fdd3e2fb36d7ff61d56ab5b65bd
Devil505 <devil505linux@gmail.com>
2011-02-06 16:15:29 +0100

    *added 2 users screenies

--- frugalware/screenshots.php
+++ frugalware/screenshots.php
@@ -182,11 +182,15 @@ $shots[gservice][2][name]="03_gservice.png";
 $shots[gservice][2][title]=gettext("gService - Minimal Interface");
 
 $shots[users][0][name]="01_users_lenezir.png";
-$shots[users][0][title]=gettext("Openbox - Lenezir");
+$shots[users][0][title]=gettext("Openbox - by Lenezir");
 $shots[users][1][name]="02_users_pingax.png";
-$shots[users][1][title]=gettext("GNOME - Pingax");
+$shots[users][1][title]=gettext("GNOME - by Pingax");
 $shots[users][2][name]="03_users_devil505.png";
-$shots[users][2][title]=gettext("XFCE - Devil505");
+$shots[users][2][title]=gettext("XFCE - by Devil505");
+$shots[users][3][name]="04_users_slown.png";
+$shots[users][3][title]=gettext("GNOME - by Slown");
+$shots[users][4][name]="05_users_botchchikii.png";
+$shots[users][4][title]=gettext("Openbox - by Botchchikii");
 
 // include the config and let's start page
 include("config.inc.php");
