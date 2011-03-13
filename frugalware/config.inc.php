ab0129efa379d266815f91a3540432af2e80b955
Devil505 <devil505linux@gmail.com>
2011-03-13 16:48:38 +0100

    * put the wiki link as main link in menu

--- frugalware/config.inc.php
+++ frugalware/config.inc.php
@@ -59,11 +59,11 @@ $menucontent .= '<a class="menu" href="' . $fwng_root . 'packages">' . gettext('
 $menucontent .= '<a class="menu" href="' . $fwng_root . 'docs">' . gettext('Documentation') . "</a> | \n";
 $menucontent .= '<a class="menu" href="http://forums.frugalware.org/">' . gettext('Discussion Forums') . "</a> | \n";
 $menucontent .= '<a class="menu" href="http://bugs.frugalware.org/">' . gettext('Bug Tracker') . "</a> | \n";
+$menucontent .= '<a class="menu" href="http://wiki.frugalware.org/">' . gettext('Wiki') . "</a> | \n";
 
 $menucontent .= '<a class="menu" href="" onclick="return clickreturnvalue()" onmouseover="dropdownmenu(this, event, \'commmenu\')">' . gettext('Community') . "</a> | \n";
 $menucontent .= '<div id="commmenu" class="menulinkcss" align="left">' . "\n";
 $menucontent .= '	<a href="http://frugalware.org/mailman/listinfo">' . gettext('Mailing Lists') . "</a>\n";
-$menucontent .= '	<a href="http://wiki.frugalware.org/">' . gettext('Wiki') . "</a>\n";
 $menucontent .= '	<a href="' . $fwng_root . 'irc">' . gettext('IRC') . "</a>\n";
 $menucontent .= '	<a href="' . $fwng_root . 'paste">' . gettext('Pastebin') . "</a>\n";
 $menucontent .= '	<a href="http://planet.frugalware.org/">' . gettext('Blogs') . "</a>\n";
