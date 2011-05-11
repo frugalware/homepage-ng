06a17daae40dc82fcfe0281ad31e5a9c8871f562
Devil505 <devil505linux@gmail.com>
2011-05-11 14:53:33 +0200

    * added flattr button

--- frugalware/header.php
+++ frugalware/header.php
@@ -40,6 +40,18 @@ $data = genHeader();
 	    });
   </script>
 
+<script type="text/javascript">
+/* <![CDATA[ */
+    (function() {
+        var s = document.createElement('script'), t = document.getElementsByTagName('script')[0];
+        s.type = 'text/javascript';
+        s.async = true;
+        s.src = 'http://api.flattr.com/js/0.6/load.js?mode=auto';
+        t.parentNode.insertBefore(s, t);
+    })();
+/* ]]> */
+</script>
+
 <?
 if($_SERVER['PHP_SELF']=="/index.php")
 	print('<link rel="alternate" type="application/rss+xml" title="RSS" href="http://'
