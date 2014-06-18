d323c192ee684f466402e23f5c21a65308bc026f
DarkNekros <darknekros@gmail.com>
2014-06-18 10:55:15 +0200

    * Add footer strings to be tranlated. * Update the list of php files to get the translation strings.

--- frugalware/footer.php
+++ frugalware/footer.php
@@ -5,11 +5,15 @@
     </div>
 
     <!-- footer start -->
-    <footer>
-        &copy; <?php echo $copydate . " " . $title; ?> - <a href="mailto:frugalware-devel@frugalware.org">Contact Developer Team</a> - <a id="top" href="javascript: toTop();">Back to top</a>
-    </footer>
+<?php
+print "\n
+    <footer>\n
+        &copy;" . "echo $copydate" . " " . "$title" . " - 
+        <a href=\"mailto:frugalware-devel@frugalware.org\">" . gettext("Contact Developer Team") . "</a> 
+        - <a id=\"top\" href=\"javascript: toTop();\">" . gettext("Back to top") . "</a>\n
+    </footer>\n";
+?>
     <!-- footer end -->
-
 
     </body>
 </html>
