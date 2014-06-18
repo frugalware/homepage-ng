2308e8fba15b4f5ad9c3e7bd8f4ae04d107c7ec6
DarkNekros <darknekros@gmail.com>
2014-06-18 12:11:55 +0200

    * Fix typo.

--- frugalware/footer.php
+++ frugalware/footer.php
@@ -8,7 +8,7 @@
 <?php
 print "\n
     <footer>\n
-        &copy;" . "echo $copydate" . " " . "$title" . " - 
+        &copy;" . $copydate . " " . $title . " - 
         <a href=\"mailto:frugalware-devel@frugalware.org\">" . gettext("Contact Developer Team") . "</a> 
         - <a id=\"top\" href=\"javascript: toTop();\">" . gettext("Back to top") . "</a>\n
     </footer>\n";
